d8cb86de72b8b7fd411c82925e74a769efb90507
yoonji <kyjkbw@gmail.com>
2024-11-05 09:49:31 +0900

    표준내장객체 활용 연습문제 풀이 추가

--- learn/1105_표준내장객체활용연습문제.js
+++ learn/1105_표준내장객체활용연습문제.js
@@ -8,9 +8,14 @@ ex) 32125 -> 52123
     15231 -> 13251 
 */
 
+//방법1
 function reverse_to_number(number) {
   return String(number).split("").reverse().join("");
 }
+
+//방법2: 화살표함수(리액트, 뷰에서 많이 사용)
+const reverse_to_number = (number) => String(number).split("").reverse().join("");
+
 
 let a = reverse_to_number(32125);
 console.log(a); // 52123
@@ -18,16 +23,39 @@ console.log(a); // 52123
 let b = reverse_to_number(13251);
 console.log(b); // 15231
 
+//--------------------------------------------------------
+
 /*
   [가장 긴 단어를 출력하기]
 
   매개변수로 전달된 문장에서 가장 긴 단어를 출력해주세요.
 
 */
-
+//방법1: reduce() 사용
 function findLongStr(str) {
   return str.split(" ").reduce((a, b) => a.length > b.length ? a : b);
+}
 
+//방법2: sort()이용해서 문자 길이로 정렬
+{
+  function findLongStr(str) {
+    // 1. split()
+    const stringToArray = str.split(" ");
+    // 2. sort()
+    const sortArray = stringToArray.sort((a, b) => b.length - a.length);
+    // 3. shift()
+    const longString = sortArray.shift();
+
+    return longString;
+  }
+}
+//메서드 체이닝 이용해서 정리된 ver
+{
+  const findLongStr = (str) =>
+    str
+      .split(" ")
+      .sort((a, b) => b.length - a.length)
+      .shift();
 }
 
 let result = findLongStr("we are the champion");
