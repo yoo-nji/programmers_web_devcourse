d1a9dedfd88b3c8310a2a84ccc727cfbdf3a7ad5
yoonji <kyjkbw@gmail.com>
2024-11-16 20:00:44 +0900

    [Node.js] 파일 읽어오기(동기, 비동기)

--- learn-node/readFile.js
+++ learn-node/readFile.js
@@ -1,9 +1,29 @@
-const fs = require('fs');
+//NodeJS 기본 동작은 비동기
+//1. 비동기로 동작함
+//end가 먼저 출력되고 이후 "file read end" 출력
+// const fs = require('fs');
 
-fs.readFile('example.txt', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(data);
-});
+// fs.readFile('./big.txt', 'utf8', (err, data) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log("file read end");
+//   // console.log(data);
+// });
+// console.log("end");
+
+//2. async 활용
+//모든 비동기는 promises를 사용해서 모듈을 불러와 줘야 됨
+const fs = require('fs').promises;
+
+async function readFileAsync() {
+  const data = await fs.readFile('./big.txt', 'utf8');
+  console.log("file read end");
+}
+async function start() {
+  await readFileAsync();
+  console.log("end");
+}
+
+start();
