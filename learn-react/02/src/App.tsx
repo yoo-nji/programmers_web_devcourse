073678d50cb131385dd2dbeb7e8dd65ccd93f256
yoonji <kyjkbw@gmail.com>
2024-11-25 12:09:04 +0900

    [react] 클래스형 컴포넌트, 함수형 컴포넌트 비교

--- learn-react/02/src/App.tsx
+++ learn-react/02/src/App.tsx
@@ -1,7 +1,15 @@
+//✅ 클래스형 컴포넌트
+//코드가 복잡함
+// import { Component } from "react";
+// class App extends Component {
+//   render() {
+//     return <h1>App</h1>;
+//   }
+// }
+// export default App;
+
+//✅ 함수형 컴포넌트
+//대부분 함수형 컴포넌트를 리액트 컴포넌트로 작성함
 export default function App() {
-  return (
-    <>
-      <h1>App</h1>
-    </>
-  );
+  return <h1>App</h1>;
 }
