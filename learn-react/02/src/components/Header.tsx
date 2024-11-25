9ee494eacbc1bfb903ad688aa858e7eb36b41bb3
yoonji <kyjkbw@gmail.com>
2024-11-25 16:42:50 +0900

    [react] classnames 라이브러리 사용

--- learn-react/02/src/components/Header.tsx
+++ learn-react/02/src/components/Header.tsx
@@ -1,10 +1,13 @@
 import styles from "./Header.module.css";
+import classNames from "classnames/bind";
 
 export default function Header() {
+  const cx = classNames.bind(styles);
   return (
     <>
       <header>
         <h1 className={styles.title}>내 웹사이트</h1>
+        <h1 className={cx("title")}>내 웹사이트</h1>
       </header>
     </>
   );
