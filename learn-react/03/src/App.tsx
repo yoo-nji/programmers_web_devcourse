44de72e97c66f22f86791cc9e21c1cf3592f6468
yoonji <kyjkbw@gmail.com>
2024-11-26 09:45:35 +0900

    [react] 3일차 리액트 생성, css 복습

---
+++ learn-react/03/src/App.tsx
@@ -0,0 +1,11 @@
+import styles from "./App.module.css";
+export default function App() {
+  return (
+    <>
+      <div>
+        <h1 className={styles.title}>hello, react!</h1>
+        <h2>react, styling!</h2>
+      </div>
+    </>
+  );
+}
