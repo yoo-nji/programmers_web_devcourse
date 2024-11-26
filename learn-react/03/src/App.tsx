8124435802512ffd7557d3a818eaac0f6f33f7b9
yoonji <kyjkbw@gmail.com>
2024-11-26 15:53:40 +0900

    [react] 폰트 사용(구글/눈누/다운로드)

--- learn-react/03/src/App.tsx
+++ learn-react/03/src/App.tsx
@@ -5,8 +5,10 @@ export default function App() {
     <>
       {/* 스타일충돌 */}
       {/* <h1 className="text-3xl underline text-2xl">App</h1> */}
-      <h1 className={twMerge("text-3xl", "underline", "text-2xl")}>App</h1>
-
+      <h1 className={twMerge("text-3xl", "underline", "text-2xl sedgwick")}>App</h1>
+      <h1 className={twMerge("text-3xl", "underline", "text-2xl black")}>App</h1>
+      <h2 className="bagelfatone text-3xl">빵폰트</h2>
+      <h2 className="daldam text-3xl">제주돌담</h2>
       <input type="text" />
     </>
   );
