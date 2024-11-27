e661bf1ec429d380f9989f3b2a2a0b2889b48563
yoonji <kyjkbw@gmail.com>
2024-11-27 16:46:56 +0900

    [react] props 연습문제 10

--- learn-react/04/src/components/Child.tsx
+++ learn-react/04/src/components/Child.tsx
@@ -4,10 +4,17 @@ interface ChildPropsType {
   // test3: string[];
   // test4: { name: string; age: number };
   // test5: boolean;
-  test6: () => void;
+  // test6: () => void;
+  // test7?: string;
+  // value: string | number | unknown[] | object;
+  // style: object;
+  header: React.ReactElement;
+  content: React.ReactElement;
+  footer: React.ReactElement;
 }
 
 export default function Child(props: ChildPropsType) {
+  // const test7 = props.test7 ?? "홍길동";
   console.log(props);
   return (
     <div className="text-3xl">
@@ -16,6 +23,13 @@ export default function Child(props: ChildPropsType) {
       {/* <h1>{props.test3}</h1> */}
       {/* <h1>{JSON.stringify(props.test4)}</h1> */}
       {/* <h1>{props.test5 ? "로그인 됨" : "로그인 안 됨"}</h1> */}
+      {/* <button onClick={props.test6}>버튼</button> */}
+      {/* <h1>{test7}</h1> */}
+      {/* <h1>{JSON.stringify(props.value)}</h1> */}
+      {/* <h1 style={props.style}>스타일</h1> */}
+      {props.header}
+      {props.content}
+      {props.footer}
     </div>
   );
 }
