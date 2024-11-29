96359d603212cfe4cb05748cdc6fb75ab4bc7161
yoonji <kyjkbw@gmail.com>
2024-11-29 10:41:00 +0900

    [react] 이미지 렌더링 방법

--- learn-react/06/src/App.tsx
+++ learn-react/06/src/App.tsx
@@ -1,11 +1,13 @@
-import tree from "./assets/images/tree.jpg";
 export default function App() {
+  let count = 0;
+  const increment = () => {
+    count += 1;
+    console.log(count);
+  };
   return (
-    <>
-      {/* 이미지 넣기 */}
-      <img src={tree} alt="" />
-      {/* 백그라운드 이미지 넣기 */}
-      <div className="w-60 h-60" style={{ background: `url(${tree})` }}></div>
-    </>
+    <div>
+      <h1>count: {count}</h1>
+      <button onClick={increment}>증가</button>
+    </div>
   );
 }
---
+++ learn-react/06/src/assets/images/images.ts
@@ -0,0 +1,7 @@
+import tree from "./tree.jpg";
+
+// export const images = {
+//   tree,
+// };
+
+export { tree };
