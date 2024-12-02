638cf984ce9209f422f8e9cdf5009c465be0db81
yoonji <kyjkbw@gmail.com>
2024-12-02 20:48:50 +0900

    [react] useReducer 이용 로그인폼

--- learn-react/07/login/src/components/Login.tsx
+++ learn-react/07/login/src/components/Login.tsx
@@ -1,33 +1,106 @@
-import { useState } from "react";
 import Input from "./html/Input";
 import Checkbox from "./html/Checkbox";
 import Button from "./html/Button";
+import { useReducer } from "react";
+
+interface ReducerState {
+  email: string;
+  password: string;
+  agree: boolean;
+  error: string;
+}
+
+type ReducerAction =
+  | { type: "SET_EMAIL"; payload: string }
+  | { type: "SET_PASSWORD"; payload: string }
+  | { type: "SET_AGREE"; payload: boolean }
+  | { type: "SET_ERROR"; payload: string };
+
+const initialState = {
+  email: "",
+  password: "",
+  agree: false,
+  error: "",
+};
+
+function reducer(state: ReducerState, action: ReducerAction): ReducerState {
+  switch (action.type) {
+    case "SET_EMAIL":
+      return { ...state, email: action.payload };
+    case "SET_PASSWORD":
+      return { ...state, password: action.payload };
+    case "SET_AGREE":
+      return { ...state, agree: action.payload };
+    case "SET_ERROR":
+      return { ...state, error: action.payload };
+    default:
+      return state;
+  }
+}
 
 export default function Login() {
-  const [agree, setAgree] = useState(false);
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const velidationForm = () => {
+    if (!state.email) {
+      dispatch({ type: "SET_ERROR", payload: "이메일을 입력해 주세요" });
+      return false;
+    } else if (!state.password) {
+      dispatch({ type: "SET_ERROR", payload: "비밀번호를 입력해 주세요" });
+      return false;
+    } else if (!state.agree) {
+      dispatch({ type: "SET_ERROR", payload: "약관 동의를 체크해주세요" });
+      return false;
+    }
+    dispatch({ type: "SET_ERROR", payload: "" });
+    return true;
+  };
+
+  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (velidationForm()) {
+      alert("로그인 성공!");
+    }
+  };
+
   return (
     <>
       <div className="item-middle bg-black">
-        <div className="w-[375px] h-[434px] rounded-lg bg-white border border-[#D1D1D1] py-10 px-[25px] text-[#4f4f4f]">
+        <div className="w-[375px] h-[480px] rounded-lg bg-white border border-[#D1D1D1] py-10 px-[25px] text-[#4f4f4f]">
           <h1 className="text-xl font-bold mb-[10px]">Login Into App</h1>
           <p className="text-sm mb-5">Please enter your details to continue.</p>
-          <form action="" className="grid gap-4">
+          <form onSubmit={submitHandler} className="grid gap-4">
             <Input
-              type="text"
+              type="email"
               className="input-style1"
               placeholder="someone@example.com"
+              value={state.email}
+              onChange={(e) =>
+                dispatch({ type: "SET_EMAIL", payload: e.target.value })
+              }
             />
             <Input
               type="password"
               className="input-style1"
               placeholder="Enter Password"
+              value={state.password}
+              onChange={(e) =>
+                dispatch({ type: "SET_PASSWORD", payload: e.target.value })
+              }
             />
-            <Checkbox checked={agree} onChange={() => setAgree(!agree)}>
+            <Checkbox
+              onChange={(e) =>
+                dispatch({ type: "SET_AGREE", payload: e.target.checked })
+              }
+              checked={state.agree}
+            >
               <span className="text-sm color-[#4f4f4f]">
                 I agree with <em className="not-italic font-bold">terms</em> and{" "}
                 <em className="not-italic font-bold">policies</em>.
               </span>
             </Checkbox>
+            {/* ⚠️ 에러메시지 */}
+            <p className="text-rose-500"> {state.error || "\u00A0"}</p>
             <div className="mt-4 grid gap-4">
               <Button
                 type="submit"
