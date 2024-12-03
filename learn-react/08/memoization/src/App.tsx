e548a2fa77beaed4506be356863f81192611de0f
yoonji <kyjkbw@gmail.com>
2024-12-03 23:45:21 +0900

    [react] 메모이제이션 복습

--- learn-react/08/memoization/src/App.tsx
+++ learn-react/08/memoization/src/App.tsx
@@ -1,16 +1,18 @@
 //React.memo() //고차함수 => 컴포넌트를 반환하는 함수
 
 import A from "./components/A";
+import { initialItems } from "./utils/utils";
 
 //리액트훅
 //useCallback()
 //useMemo()
 export default function App() {
   console.log("App Rendering");
-
+  const selectedItems = initialItems.find((item) => item.selected);
+  console.log(selectedItems);
   return (
     <>
-      <h1>APP</h1>
+      <h1>APP: {selectedItems?.id}</h1>
       <A />
     </>
   );
