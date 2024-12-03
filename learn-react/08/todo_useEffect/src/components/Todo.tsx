8996406912c7942cdcb16c5e3e672e59a9fd73ec
yoonji <kyjkbw@gmail.com>
2024-12-03 12:53:26 +0900

    [react] todo 디벨롭 -> useEffect 이용 localStorage 저장

--- learn-react/08/todo_useEffect/src/components/Todo.tsx
+++ learn-react/08/todo_useEffect/src/components/Todo.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TodoEditor from "./TodoEditor";
 import TodoHeader from "./TodoHeader";
 import TodoList from "./TodoList";
@@ -7,7 +7,10 @@ import TodoList from "./TodoList";
 // 최상위 스코프 레벨에서만 써야함
 // 함수 바로 안에서만 써야함
 export default function Todo() {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  //초기값 변경
+  const [todos, setTodos] = useState<Todo[]>(
+    JSON.parse(localStorage.getItem("todos") || "[]")
+  );
   const addTodo = (text: string) => {
     setTodos((todos) => [
       ...todos,
@@ -24,6 +27,10 @@ export default function Todo() {
   const removeTodo = (id: number) => {
     setTodos((todos) => todos.filter((todo) => todo.id !== id));
   };
+  // 웹브라우저 로컬스토리지에 저장
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
   return (
     <div className="max-w-md mx-auto shadow-lg rounded-lg overflow-hidden">
       <TodoHeader />
