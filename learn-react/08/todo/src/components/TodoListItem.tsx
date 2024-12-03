7c4f7dbb7f41083bad506bc64c6f1d8a5fc4aebc
yoonji <kyjkbw@gmail.com>
2024-12-03 10:27:35 +0900

    [react] useId 사용

--- learn-react/08/todo/src/components/TodoListItem.tsx
+++ learn-react/08/todo/src/components/TodoListItem.tsx
@@ -1,3 +1,5 @@
+import { useId } from "react";
+
 export default function TodoListItem({
   todo,
   removeTodo,
@@ -7,18 +9,22 @@ export default function TodoListItem({
   removeTodo: (id: number) => void;
   toggleTodo: (id: number) => void;
 }) {
+  //!! 고유한 id값을 생성할 때 쓰는 훅
+  const uuid = useId();
   return (
     <li className="flex items-center justify-between p-3 border-b hover:bg-gray-100 transition-colors">
       <div className="flex items-center">
         <input
+          id={uuid}
           type="checkbox"
           className="mr-3 h-4 w-4 text-blue-500 focus:ring-blue-400"
           checked={todo.completed}
           onChange={() => toggleTodo(todo.id)}
         />
-        <span className={`${todo.completed && "line-through"}`}>
+        <label htmlFor={uuid} className={`${todo.completed && "line-through"}`}>
+          {" "}
           {todo.text}
-        </span>
+        </label>
       </div>
       <button
         className="text-red-500 hover:text-red-700 ml-4"
