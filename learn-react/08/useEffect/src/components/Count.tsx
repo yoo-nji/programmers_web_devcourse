04e28d4d2fc673aac68b56ab89241f737bdcac45
yoonji <kyjkbw@gmail.com>
2024-12-03 12:52:54 +0900

    [react] useEffect

--- learn-react/08/useEffect/src/components/Count.tsx
+++ learn-react/08/useEffect/src/components/Count.tsx
@@ -1,6 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Count() {
+  const [count, setCount] = useState(0);
+  let count2 = 0;
   //useEffect(콜백함수, 의존성배열)
 
   //✅ 생성
@@ -10,14 +12,26 @@ export default function Count() {
     //보통 api 호출을 함
     console.log("App Created");
 
+    const interval = setInterval(() => {
+      count2 += 1;
+      console.log("interval!!");
+    }, 1000);
+
     //삭제될 때 호출됨
     return () => {
       console.log("Count Destroyed");
+      clearInterval(interval);
     };
   }, []);
+
+  useEffect(() => {
+    //useEffect는 무조건 한 번 호출되기 떄문에 처음에 호출 안 하려면 이렇게
+    if (count > 0) console.log("count updated:" + count);
+  }, [count]);
   return (
     <>
-      <h1>Count</h1>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount((count) => count + 1)}>상태 증가</button>
     </>
   );
 }
