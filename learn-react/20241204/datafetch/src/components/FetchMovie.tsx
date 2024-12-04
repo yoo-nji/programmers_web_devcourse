454b061a1abf7f473d12cb7fc709be8266e452df
yoonji <kyjkbw@gmail.com>
2024-12-04 21:44:58 +0900

    환경변수 추가

--- learn-react/20241204/datafetch/src/components/FetchMovie.tsx
+++ learn-react/20241204/datafetch/src/components/FetchMovie.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect } from "react";
 
 export default function FetchMovie() {
+  const API_KEY = import.meta.env.VITE_REACT_APP_API_KEY;
   const getMovie = async () => {
     const { data } = await axios.get(
       "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
@@ -9,8 +10,7 @@ export default function FetchMovie() {
         method: "GET", //대문자
         headers: {
           accept: "application/json",
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMGY5ZGVmN2EzMzJhNTEyZmIwZDhlMzJiOTRkNWNmNCIsIm5iZiI6MTcyMjMyMzYwOS42MjEsInN1YiI6IjY2YTg5Mjk5MDBhZjMwNThjZDRiYmQ1YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.buhkGGYszxQXky_cDlxEa_iyk8Twd9ZLcTs_LMmJVnc",
+          Authorization: `Bearer ${API_KEY}`,
         },
       }
     );
