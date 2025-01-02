852a7c00c3cb69e0b38bc437e9540b0f4327091b
yoonji <kyjkbw@gmail.com>
2025-01-02 11:41:46 +0900

    [vue] vue 기본 문법1

--- learn-vue/20240102/src/App.vue
+++ learn-vue/20240102/src/App.vue
@@ -1,47 +1,58 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+//옵션스API
+export default {
+  name: 'App', //컴포넌트 이름, 왜 적나? 디버깅을 쉽게 하기 위해서(권장)
+  data () {
+    return {
+      message: 'Hello, world!',
+      name: '철수',
+      age: 20,
+      likes: ['apple', 'banana', 'cherry'],
+      user: {
+        name: '영희',
+        age: 25,
+      },
+      id: 'main',
+      class: 'red-color',
+      isActive: true,
+    }; //데이터를 리턴하는 함수, 객체를 리턴해야함 이걸 템플릿에서 사용 가능함
+  }, // 뷰에서 사용할 데이터를 정의하는 것, vue에서 사용하는 데이터는 무조건 data 속성으로 정의함
+};
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
+  <!-- vue 주요 문법 -->
+  <!-- ✅ 1. 데이터 보간법, {{  }} (콧수염 문법, mustach syntax) -->
+  <h1>{{ message }}</h1>
+  <h1>{{ name }}</h1>
+  <h1>{{ age }}</h1>
+  <!-- React와 차이점 배열, 객체 데이터를 있는 그대로 보여줌 -->
+  <h1>{{ likes[2] }}</h1>
+  <h1>{{ user }}</h1>
+  <!--❌ 표현식 내에서 간단한 자바스크립트 문법을 넣어줄 수도 있다, 하지만 권장되지 않음 -->
+  <h1>{{ 10 + 30 }}</h1>
+  <h1>{{ message.toUpperCase() }}</h1>
+  <hr />
+  <!--✅ 2. 디렉티브: 뷰에서만 사용할 수 있는 특별한 '속성', v-[] -->
+  <h1 v-html="message"></h1>
+  <h1 v-text="message"></h1>
+  <h1 v-pre="message"></h1>
+  <hr />
+  <!-- v-bind -->
+  <!-- 단축문법(축약속성)제공, v-bind 대신 ':' 콜론 이용, 넣어야 하는 속성과 이름이 같아도 축약 가능함 -->
   <main>
-    <TheWelcome />
+    <h1>Hello</h1>
+    <h1 :class="isActive ? 'active' : 'inactive' ">동적 클래스</h1>
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style>
+.red-color {
+  color: red;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.active {
+  background-color: yellow;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.inactive {
+  background-color: gray;
 }
 </style>
--- learn-vue/20240102/src/main.js
+++ learn-vue/20240102/src/main.js
@@ -1,6 +1,18 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+const app = createApp(App);
+app.mount('#app');
+//이렇게 끊어서 작성할 수도 있음
 
-createApp(App).mount('#app')
+//✅ 객체 형식으로도 전달 가능함
+// import { createApp, h } from 'vue';
+// import App from './App.vue';
+// const app = createApp({
+//   render() {
+//     return h('h1', 'Hello World!');
+//   }
+// });
+
+// 💡 원래는 이렇게 해야 하지만 얘를 조금 더 간단하게 쓸 수 있는 방법
+// => vue라는 확장자를 렌더링하는 방식으로 사용할 수 있음
