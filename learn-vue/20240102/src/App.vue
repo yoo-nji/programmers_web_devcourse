b8f9f95a1c1f60bcdcf8ec27663282f0ce705d76
yoonji <kyjkbw@gmail.com>
2025-01-02 14:05:05 +0900

    [vue] vue 기본 문법3

--- learn-vue/20240102/src/App.vue
+++ learn-vue/20240102/src/App.vue
@@ -3,27 +3,19 @@ export default {
   name: 'App',
   data() {
     return {
-      imageUrl: "https://pixabay.com/ko/photos/%EC%B9%B8%EB%8D%B8%EB%9D%BC-%EB%B0%94%EB%8B%A4-%EC%B2%9C%EA%B5%AD-%ED%95%B4%EB%B3%80-%EB%AC%BC-6826697/",
-      imageSrc: "https://cdn.pixabay.com/photo/2021/11/26/20/45/lantern-6826697_1280.jpg",
-      class: "image",
-      styledObject: {
-        color: "red",
-      textDecoration: "underline",
-    },
-  }
-}
-}
+
+    };
+  },
+};
 </script>
 <template>
-  <div>
-    <a :href="imageUrl"> <img :src="imageSrc" :class alt="altText" /></a>
-  </div>
-  <!-- ✅ 스타일 속성도 v-bind로 가능 -->
-  <h1 :style="styledObject">스타일 속성</h1>
-</template>
+  <h1 v-if="true">true</h1>
+  <h1 v-if="false">false</h1>
 
-<style>
-.image{
-  width: 100%;
-}
-</style>
+  <h1 v-show="true">true</h1>
+  <h1 v-show="false">false</h1>
+  <!-- v-if, v-show 차이점 -->
+  <!-- v-if는 조건이 false일 경우 해당 엘리먼트를 제거 -->
+  <!-- v-show는 조건이 false일 경우 해당 엘리먼트를 display: none 처리 -->
+</template>
+<style scoped></style>
