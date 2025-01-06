b14d3436d2b8cfa071eac3fb14538c9a722ad3ec
yoonji <kyjkbw@gmail.com>
2025-01-06 11:15:22 +0900

    [vue] v-memo

--- learn-vue/20250106/src/App.vue
+++ learn-vue/20250106/src/App.vue
@@ -5,6 +5,9 @@ export default {
     return {
       count: 0,
       query: "",
+      name: "홍길동",
+      age: 20,
+      gender: "male",
     };
   },
   methods: {
@@ -48,5 +51,15 @@ export default {
 
   <input type="text" :value="query" @input="handleInput" />
   {{ query }}
+  <hr />
+  <!-- 메모이제이션 -->
+  <div v-memo="[name, age]">
+    <h1>{{ name }}</h1>
+    <h1>{{ age }}</h1>
+    <h1>{{ gender }}</h1>
+  </div>
+  <button @click="name = '유리'">이름 변경</button>
+  <button @click="age = 5">나이 변경</button>
+  <button @click="gender = 'female'">성별 변경</button>
 </template>
 <style scoped></style>
