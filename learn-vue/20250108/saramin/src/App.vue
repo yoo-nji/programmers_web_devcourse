97faee4e8b54015fa6941f3b03f20bb501c7ce63
yoonji <kyjkbw@gmail.com>
2025-01-08 09:26:36 +0900

    [vue] 사람인 과제 프로젝트 세팅

---
+++ learn-vue/20250108/saramin/src/App.vue
@@ -0,0 +1,85 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <div class="saramin">
+    <h1>글자수세기</h1>
+    <div class="box">
+      <div class="string-length">
+        <textarea placeholder="내용을 입력해주세요"></textarea>
+      </div>
+      <div class="str-info">
+        <p>공백 포함 <span>0</span> 자 | <span>0</span> byte</p>
+        <p>공백 제외 <span>0</span> 자 | <span>0</span> byte</p>
+      </div>
+      <div class="btn-area">
+        <button>전체복사</button>
+        <button>초기화</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.saramin {
+  width: 564px;
+  margin: 100px auto;
+}
+
+.saramin .box {
+  border-top: 1px solid #676767;
+  border-left: 1px solid #ddd;
+  border-right: 1px solid #ddd;
+  border-block: 1px solid #ddd;
+}
+
+.saramin h1 {
+  font-size: 32px;
+  margin-bottom: 50px;
+  line-height: 1;
+  font-weight: normal;
+}
+
+.saramin .string-length {
+  padding: 30px;
+}
+
+.saramin .string-length textarea {
+  width: 100%;
+  height: 400px;
+  border: none;
+}
+
+.saramin .str-info {
+  border-top: 1px solid #ebebeb;
+  padding: 0 22px;
+  font-size: 14px;
+  margin: 0px 8px;
+}
+
+.saramin .str-info span {
+  font-size: 15px;
+  color: #ff662f;
+  font-weight: bold;
+}
+
+.saramin .btn-area {
+  background-color: #fbfbfb;
+  padding: 35px 0;
+  text-align: center;
+}
+
+.saramin .btn-area button {
+  background: transparent;
+  border: 1px solid #d9d9d9;
+  width: 160px;
+  height: 40px;
+  margin: 0 2px;
+}
+
+.saramin .btn-area button:nth-child(1) {
+  background-color: #9c9c9c;
+  color: white;
+}
+</style>
