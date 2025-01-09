61eaaaf84892dfb1021b06fbb5a73f3532185afd
yoonji <kyjkbw@gmail.com>
2025-01-09 12:51:09 +0900

    [vue] 컴포지션 watch

--- learn-vue/20250109/src/components/part/SetupP.vue
+++ learn-vue/20250109/src/components/part/SetupP.vue
@@ -1,5 +1,6 @@
 <script setup>
 // import { reactive, ref } from "vue";
+import { reactive } from "vue";
 import Setup from "../Setup.vue";
 
 // 반응형 데이터 정의
@@ -12,7 +13,7 @@ const state = ref({
   count: 0,
 });
 const numArr = ref([1, 2, 3, 4, 5]);
-console.log(count.value);
+// console.log(count.value);
 
 const state2 = reactive({
   count: 0,
@@ -20,9 +21,9 @@ const state2 = reactive({
 
 // 둘은 차이가 없어 보이지만 스크립트 태그 내에서 사용할 땐 차이가 있다
 // ref는 .value를 붙여야 실제 값을 사용할 수 있지만 reactive는 그냥 사용 가능
-console.log(state.value.count);
-console.log(count.value);
-console.log(state2.count);
+// console.log(state.value.count);
+// console.log(count.value);
+// console.log(state2.count);
 
 //computed
 const doubleCount = computed(() => count.value * 2);
@@ -37,6 +38,44 @@ const fullName = computed({
     lastName.value = names[1];
   },
 });
+
+// watch 감시자
+// 특정 데이터를 감시
+const count2 = ref(0);
+
+const state3 = ref({
+  num: 0,
+});
+
+const incrementState3 = () => {
+  state3.value.num++;
+};
+
+watch(count2, (cur, prev) => {
+  console.log("count2 변경됨", cur, prev);
+});
+watch(
+  state3,
+  (cur, prev) => {
+    console.log("state3 변경됨", cur, prev);
+  },
+  {
+    deep: true,
+  }
+);
+
+const state4 = reactive({
+  num: 0,
+});
+const incrementState4 = () => {
+  state4.num++;
+};
+watch(
+  () => state4.num,
+  (cur, prev) => {
+    console.log("state4 변경됨", cur, prev);
+  }
+);
 </script>
 <template>
   <h1>Setup</h1>
@@ -65,5 +104,20 @@ const fullName = computed({
   <h3>computed2</h3>
   <div>{{ fullName }}</div>
   <button @click="fullName = '김 철수'">이름 변경</button>
+
+  <hr />
+  <h3>watch</h3>
+  <div>{{ count2 }}</div>
+  <button @click="count2++">증가(count2)</button>
+
+  <hr />
+  <h3>watch2 - ref로 반응형 데이터</h3>
+  <div>{{ state3.num }}</div>
+  <button @click="incrementState3">증가(state3.num)</button>
+
+  <hr />
+  <h3>watch3 - reactive로 반응형 데이터</h3>
+  <div>{{ state4.num }}</div>
+  <button @click="incrementState4">증가(state4.num)</button>
 </template>
 <style scoped></style>
